6c630abaa40d90f8ef52d813ee226e315b1ee197
Joey Scott <joannascott29@hotmail.co.uk>
2017-03-20 15:10:02 +0000

    Add validateage.js

--- modules/validateage.js
+++ modules/validateage.js
@@ -1,5 +1,12 @@
 // @TODO: Build validateAge
 // Integer (must be greater than 16 but less than 80)
-const validateAge = () =>
+const validateAge = (age) => {
+  if (age <=16 || age >= 80) {
+    return new Error(`${age}: You are too young or too old!`);
+  }
+  else if ((typeof age !== 'number') && (age % 1 === 0)) {
+    return new Error(`${age} is not an integer`);
+  }
+}
 
 module.exports = validateAge;
