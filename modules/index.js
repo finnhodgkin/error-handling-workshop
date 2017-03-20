9697c154d53a91b110c4efb9a39822189706acd2
Joey Scott <joannascott29@hotmail.co.uk>
2017-03-20 15:57:55 +0000

    Add validateName

--- modules/index.js
+++ modules/index.js
@@ -3,9 +3,9 @@ const validateAge = require('./validateage');
 const validateEmail = require('./validateemail');
 const validateReason = require('./validatereason');
 
-const val1 = validateEmail('finnhodgkin@gmail.com....');
+const val1 = validateName('finnhodgkin@com');
 
-if (typeof val1 === 'error') {
+if (val1 instanceof Error) {
   console.log(val1.message);
 } else {
   console.log(val1);
--- modules/validatename.js
+++ modules/validatename.js
@@ -1,5 +1,17 @@
 // @TODO: Build validateName
 // String (characters allowed: letter, dash, apostrophe. Must contain at least two letters)
-const validateName = () =>
-
+const validateName = (name) => {
+  if (typeof name !== 'string') {
+    return new Error(`${name}: not a string!`);
+  }
+  else if (name.length < 2) {
+    return new Error(`${name}: is not long enough!`);
+  }
+  else if (!name.match(/^[a-z\-']*$/gim)) {
+    return new Error(`${name} is not a valid name`);
+  }
+  else {
+    return name;
+  }
+}
 module.exports = validateName;
